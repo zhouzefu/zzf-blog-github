083069f0392c04049cf72d94dd2cb27e75a78b5f
zhouzefu <zhouzefu@126.com>
2023-12-14 08:50:07 +0800

    首屏

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 /**
  * 邮箱地址
  */
-const email = 'zhouzefu@126.com'; 
+const email = 'zhouzefu@126.com';
 
-const status = {
-    
+/**
+ * 站点描述
+ */
+const website_remark = '这是一个GitHub站点，项目通过Ngrok部署在本地Linux服务器';
+
+/**
+ * 项目状态
+ */
+const project_phase = {
+    /**
+     * 规划中
+     */
+    planning: 0,
+    /**
+     * 开发中
+     */
+    developing: 1,
+    /**
+     * 测试中
+     */
+    testing: 2,
+    /**
+     * 运行中
+     */
+    running: 3
 };
 
 /**
@@ -14,7 +37,7 @@ const projects = [{
     id: 1,
     title: '个人博客',
     remark: '我的个人博客技术文章等',
-    // status:
+    status: project_phase.planning
 }]
 
 /**
@@ -28,4 +51,15 @@ window.onload = res => initPageData(res);
  * 初始化页面数据
  * @param {Event} res 
  */
-function initPageData(res) { }
+function initPageData(res) {
+    $('email').innerHTML = email;
+}
+
+/**
+ * 通过ID获取DOM元素
+ * @param {string} id 
+ * @returns 
+ */
+function $(id) {
+    return window.document.getElementById(id);
+}
