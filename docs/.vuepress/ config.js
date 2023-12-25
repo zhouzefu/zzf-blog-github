466a6a6b6d4d3dcd1dec3c59ea97674067b30083
zhouzefu <zhouzefu@126.com>
2023-12-25 17:03:22 +0800

    初始化项目

---
+++ docs/.vuepress/ config.js
@@ -0,0 +1,8 @@
+/**
+ * vuepress 配置
+ * @type {{description: string, title: string}}
+ */
+module.exports = {
+    title: 'zhouzefu@126.com',
+    description: '周泽富的个人博客'
+}
---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,10 @@
+/**
+ * 应用级别的配置
+ * @param Vue VuePress 正在使用的 Vue 构造函数
+ * @param options 附加到根实例的一些选项
+ * @param router 当前应用的路由实例
+ * @param siteData 站点元数据
+ * @param isServer 当前应用配置是处于 服务端渲染 或 客户端
+ */
+export default ({Vue, options, router, siteData, isServer }) => {
+}
